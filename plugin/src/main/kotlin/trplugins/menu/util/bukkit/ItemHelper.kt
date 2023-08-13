cd46d7ab654b7840d6125891f3678f4ea41d3b61
shuiqing2046 <28628358+shuiqing2046@users.noreply.github.com>
2023-08-13 08:21:13 +0800

    [3.1.29] Update TabooLib(6.0.12-9) > 1.20.1 Support

--- plugin/src/main/kotlin/trplugins/menu/util/bukkit/ItemHelper.kt
+++ plugin/src/main/kotlin/trplugins/menu/util/bukkit/ItemHelper.kt
@@ -109,7 +109,7 @@ object ItemHelper {
                     itemStack?.amount = it.asInt
                 }
                 val meta = parse["meta"]
-                return if (meta != null) itemStack.also { ItemTag.fromLegacyJson(meta.toString()).saveTo(it) }
+                return if (meta != null) itemStack.also { ItemTag.fromLegacyJson(meta.toString()).saveTo(it!!) }
                 else itemStack
             }
             return null
